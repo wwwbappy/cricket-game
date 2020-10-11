db61f58b6e7105a10ca77eb22e63a10751b22585
wwwbappy <71563526+wwwbappy@users.noreply.github.com>
2020-10-11 08:05:16 -0700

    myfirst
    my first cricket game

---
+++ script.js
@@ -0,0 +1,39 @@
+var c= document.createElement("canvas");
+var ctx=c.getContext("2d")
+c.width=500;
+c.height=350;
+document.body.appendChild(c);
+
+var perm = [];
+while (perm.length<255);{
+    while(perm.includes(val=Math.floor(Math.random()*255)));
+    perm.push(val)
+}
+
+   
+
+var lerp = (a,b,t) => a + (b-a)*t;
+var noise = x =>{
+    return(perm[Math.floor(x)],perm[Math.ceil(x)],x-Math.floor(x));
+}
+
+
+
+
+
+function loop(){
+    ctx.fillStyle="#19f";
+    ctx.fillRect(0,0,c.width,c.height);
+
+    ctx.fillStyle="black";
+    ctx.beginPath();
+    for(i-0; i<c.width;i++);
+        ctx.lineTo(i,noise(i))
+
+
+    ctx.fill(loop);
+
+   
+}
+
+loop();
